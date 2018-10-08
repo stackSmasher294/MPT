bb33d29b14718db55b3c128ab154ffa30a59212e
Shubham Paul <shubhampaul14@gmail.com>
2018-10-09 01:47:22 +0530

    Added portfolio mean and variance calculation

--- mpt.py
+++ mpt.py
@@ -58,21 +58,42 @@ class Porfolio(object):
     def __init__(self, stock_list_obj):
         self.stock_list = stock_list_obj
         self.n_stocks = len(stock_list_obj)
+        # assign equal weights to all the stocks to start with .. 
+        self.weight_list = [1./self.n_stocks for i in range(self.n_stocks)]
         self.mean_vector = np.array([stock.get_mean_variance()[0] for stock in self.stock_list])
         self.return_matrix = np.array([ stock.returns_list for stock in self.stock_list ])
+        print np.shape(self.return_matrix)
         self.returns_covariance = np.cov(self.return_matrix)
-        # print self.returns_covariance
+        print self.returns_covariance
 
     def set_portfolio_weights(self, weight_list):
         self.weight_list = weight_list
 
-    def get_portfolio_return(self):
+    def get_portfolio_mean(self):
         self.weight_vector = np.array([self.weight_list])
         expected_return = np.matmul(self.mean_vector, self.weight_vector.T)
-        
+        return expected_return[0] # only one element
+
+    def get_portfolio_variance(self):
+        N, N = np.shape(self.returns_covariance) 
+        # Calculate a mask which has to be dot multiplied with the covariance matrix
+        mask = np.zeros((N, N))
+        for i in range(N):
+            for j in range(N):
+                mask[i][j] = self.weight_list[i] * self.weight_list[j]
+        # dot multiply mask with covariance
+        weighted_covariance = mask * self.returns_covariance
+        return np.sum(weighted_covariance)
+
+    def get_portfolio_performance(self, weight_list):
+        self.set_portfolio_weights(weight_list)
+        mean = self.get_portfolio_mean()
+        var = self.get_portfolio_variance()
+        return mean, var
+
 
 if __name__ == '__main__':
-    root_dir = 'weekly'
+    root_dir = 'daily'
     csv_list = os.listdir(root_dir)
 
     stocklist = []
@@ -84,7 +105,11 @@ if __name__ == '__main__':
         print stock.name
         print '\tMean, Variance: {}'.format(stock.get_mean_variance())
         print '\tNet return: {}'.format(stock.get_net_return())
+        print '\tpoints: {}'.format(len(stock.returns_list))
         # stock.show_closing_prices()
         # stock.show_distribution()
     portfolio = Porfolio(stocklist)
+
+    print portfolio.get_portfolio_performance([0.25, 0.25, 0.25, 0.25])
+    print portfolio.get_portfolio_performance([0.1, 0.2, 0.3, 0.4])
     
